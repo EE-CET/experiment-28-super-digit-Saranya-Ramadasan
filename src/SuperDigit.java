c3c60bd28f316a5f1e4a707619a3eb195de15ffe
Saranya-Ramadasan <rsnyaramadasan04@gmail.com>
2026-04-08 17:06:15 +0530

    2nd commit

--- src/SuperDigit.java
+++ src/SuperDigit.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class SuperDIgit {
+public class SuperDigit {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
